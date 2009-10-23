8e72fb358a758570764cbaa03464fb0a86f6452e
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2009-10-23 14:37:24 +0200

    Twig started

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,25 @@
 <?php
 
+
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+        protected function _initAutoload ()
+        {
+            $moduleLoader = new Zend_Application_Module_Autoloader(
+            array('namespace' => '' , 'basePath' => APPLICATION_PATH));
+            $moduleLoader->addResourceType('formax', '../library/Twig', 'Twig');
+            return $moduleLoader;
+        }
+
+	public function _initView()
+	{	
+       	        $view = new Twig_Adapter_Zend();
+	        $view->setScriptPath(APPLICATION_PATH . '/views/scripts/');
+
+                $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+                $viewRenderer->setView($view); 
+	}
 
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,15 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $this->view->var = "Test's & Test";
+
+        $navigation = array(
+                            array("href" => "http://framework.zend.com", "caption" => "Zend Framework"),
+                            array("href" => "http://www.zend.com", "caption" => "Zend")
+                            );
+
+
+        $this->view->navigation = $navigation;
     }
 
 
